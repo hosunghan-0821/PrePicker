6e1953e370c708bf69e1ae29530fb6caddea05bf
hosung <winsomed96@naver.com>
2023-09-02 12:00:36 +0900

    fe: add  global ResponseDto

--- Backend/src/main/java/co/kr/demo/api/order/orderController.java
+++ Backend/src/main/java/co/kr/demo/api/order/orderController.java
@@ -1,10 +1,12 @@
 package co.kr.demo.api.order;
 
 
+import co.kr.demo.service.dto.ResponseDto;
 import co.kr.demo.service.dto.viewDto.OrderViewDto;
 import co.kr.demo.service.order.Interface.IOrderFacade;
 import co.kr.demo.service.dto.domainDto.OrderDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,10 @@ public class orderController {
     private final IOrderFacade orderFacade;
 
     @PostMapping("/product")
-    public void createProduct(@RequestBody OrderViewDto orderViewDto){
+    public ResponseEntity<ResponseDto<OrderViewDto>> createProduct(@RequestBody OrderViewDto orderViewDto){
 
         //Validation 작업 진행
         orderFacade.registerOrder(orderViewDto);
-
+        return ResponseEntity.ok(ResponseDto.response(orderViewDto));
     }
 }
